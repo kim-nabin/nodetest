dcbf9679db50f95bb87d2e82acf437d101062f7b
guma <skqlsrla@naver.com>
2024-09-30 13:37:38 +0900

    request test

--- api_server.js
+++ api_server.js
@@ -1,4 +1,6 @@
-
+// 서치파라미터 받기
+// 시간차이 테이블 저장
+// 파라미터/시작타임/끝타임/걸린시간
 const express = require('express');
 const { Client } = require('pg');
 
@@ -24,21 +26,24 @@ client.connect().catch(err => console.error('Connection error', err.stack));
 // SQL 쿼리 실행 API
 app.post('/execute-sql', async (req, res) => {
   const sql = req.body.sql;
-  console.log('Start time:', new Date());
+  
 
   try {
+    const start_time = new Date();
+    console.log('Start time:', start_time);
     const result = await client.query(sql);
     console.log('Query result:', result.rows);
     res.json(result.rows);
+    const end_time = new Date();
+    console.log('End time:', end_time);
+    console.log('Execution time:', (end_time - start_time)/1000);
   } catch (err) {
     console.error('Error executing SQL:', err);
     res.status(500).send('Error executing SQL query');
-  } finally {
-    console.log('End time:', new Date());
-  }
+  } 
 });
 
 // 서버 시작
-app.listen(port, () => {
-  console.log(`API server running at http://localhost:${port}`);
+app.listen(server_port, () => {
+  console.log(`API server running at http://localhost:${server_port}`);
 });
