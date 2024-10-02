958573174df44decab8f34c49c22434cebd61afe
guma <skqlsrla@naver.com>
2024-10-02 13:52:40 +0900

    pooltest

--- api_server.js
+++ api_server.js
@@ -2,7 +2,7 @@
 // 시간차이 테이블 저장
 // 파라미터/시작타임/끝타임/걸린시간
 const express = require('express');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const app = express();
 const server_port = 3000;
@@ -15,16 +15,17 @@ const date_to_str=(date) => {
 app.use(express.json());
 
 // PostgreSQL 클라이언트 설정
-const client = new Client({
+const client = new Pool({
   user: 'gutest',
   host: 'localhost',
   database: 'test_database',
   password: '1234',
   port: db_port,
+  max:100,
 });
 
 // 클라이언트 연결
-client.connect().catch(err => console.error('Connection error', err.stack));
+// client.connect().catch(err => console.error('Connection error', err.stack));
 
 // SQL 쿼리 실행 API
 app.post('/execute-sql', async (req, res) => {
@@ -34,13 +35,19 @@ app.post('/execute-sql', async (req, res) => {
   
 
   try {
+    //시작
     const start_time = new Date();
     console.log('Start time:', start_time);
+
+    //쿼리
     const result = await client.query(sql);
     console.log('Query result:', result.rows);
     res.json(result.rows);
+
+    //종료
     const end_time = new Date();
     console.log('End time:', end_time);
+
     console.log('Execution time:', (end_time - start_time)/1000);
 
     const logsql=`INSERT INTO api_testtable (test_title, start_time, end_time, execution_time) VALUES ('${test_title}_${test_seq}', '${date_to_str(start_time)}', '${date_to_str(end_time)}', ${(end_time - start_time)/1000});`
