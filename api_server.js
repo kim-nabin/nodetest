10e0fc22b4bcf60cb1045e44ece2d42a371d27b5
guma <skqlsrla@naver.com>
2024-10-02 17:12:15 +0900

    순차실행, 데이터 누락 없음

--- api_server.js
+++ api_server.js
@@ -1,8 +1,5 @@
-// 서치파라미터 받기
-// 시간차이 테이블 저장
-// 파라미터/시작타임/끝타임/걸린시간
 const express = require('express');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const app = express();
 const server_port = 3000;
@@ -14,6 +11,18 @@ const date_to_str=(date) => {
 // JSON 요청 본문을 파싱하기 위한 미들웨어
 app.use(express.json());
 
+// PostgreSQL 클라이언트 설정
+const client = new Pool({
+  user: 'gutest',
+  host: 'localhost',
+  database: 'test_database',
+  password: '1234',
+  port: db_port,
+ 
+});
+
+// 클라이언트 연결
+// client.connect().catch(err => console.error('Connection error', err.stack));
 
 // SQL 쿼리 실행 API
 app.post('/execute-sql', async (req, res) => {
@@ -21,28 +30,18 @@ app.post('/execute-sql', async (req, res) => {
   const test_seq=req.query.seq || null;
   const sql = req.body.sql;
   
-  // 클라이언트 연결
-  // client.connect().catch(err => console.error('Connection error', err.stack));
-
-  // PostgreSQL 클라이언트 설정
-  const client = new Client({
-    user: 'gutest',
-    host: 'localhost',
-    database: 'test_database',
-    password: '1234',
-    port: db_port,
-  });
 
   try {
-    await client.connect();
-
+    //시작
     const start_time = new Date();
     console.log('Start time:', start_time);
 
+    //쿼리
     const result = await client.query(sql);
-    // console.log('Query result:', result.rows);
+    console.log('Query result:', result.rows);
     res.json(result.rows);
 
+    //종료
     const end_time = new Date();
     console.log('End time:', end_time);
 
@@ -53,9 +52,7 @@ app.post('/execute-sql', async (req, res) => {
   } catch (err) {
     console.error('Error executing SQL:', err);
     res.status(500).send('Error executing SQL query');
-  } finally{
-    await client.end();
-  }
+  } 
 });
 
 // 서버 시작
